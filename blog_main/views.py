60b0d5f1a6756c39e60f40d0665fa23bb61a1e21
SHRUTI KUMARI <sk2050993@gmail.com>
2026-03-15 22:33:54 +0530

    category crud functionality

--- blog_main/views.py
+++ blog_main/views.py
@@ -34,7 +34,7 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('register')
+            return redirect('login')
         else:
             print(form.errors)
         
